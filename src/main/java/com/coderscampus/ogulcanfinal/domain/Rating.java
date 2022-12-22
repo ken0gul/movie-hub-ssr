31ca943b3d6efb6c0b74d7df4706524f9fc64cbd
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-22 18:25:35 -0500

    Create Rating.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/Rating.java
@@ -0,0 +1,19 @@
+package com.coderscampus.ogulcanfinal.domain;
+
+public class Rating {
+	private String source;
+	private String value;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
