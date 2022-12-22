26e93bf01a367d8fc209beb14776f250fea7e2e5
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-22 18:20:30 -0500

    Create Movie.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/Movie.java
@@ -0,0 +1,175 @@
+package com.coderscampus.ogulcanfinal.domain;
+
+public class Movie {
+
+	private String title;
+	private String year;
+	private String rated;
+	private String released;
+	private String runtime;
+	private String Genre;
+	private String Director;
+	private String Writer;
+	private String Actors;
+	private String Plot;
+	private String Language;
+	private String country;
+	private String awards;
+	private String poster;
+	private Rating[] ratings;
+	private String metascore;
+	private String imdbRating;
+	private String imdbID;
+	private String type;
+	private String dvd;
+	private String boxOffice;
+	private String production;
+	private String website;
+	private String response;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getRated() {
+		return rated;
+	}
+	public void setRated(String rated) {
+		this.rated = rated;
+	}
+	public String getReleased() {
+		return released;
+	}
+	public void setReleased(String released) {
+		this.released = released;
+	}
+	public String getRuntime() {
+		return runtime;
+	}
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	public String getGenre() {
+		return Genre;
+	}
+	public void setGenre(String genre) {
+		Genre = genre;
+	}
+	public String getDirector() {
+		return Director;
+	}
+	public void setDirector(String director) {
+		Director = director;
+	}
+	public String getWriter() {
+		return Writer;
+	}
+	public void setWriter(String writer) {
+		Writer = writer;
+	}
+	public String getActors() {
+		return Actors;
+	}
+	public void setActors(String actors) {
+		Actors = actors;
+	}
+	public String getPlot() {
+		return Plot;
+	}
+	public void setPlot(String plot) {
+		Plot = plot;
+	}
+	public String getLanguage() {
+		return Language;
+	}
+	public void setLanguage(String language) {
+		Language = language;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAwards() {
+		return awards;
+	}
+	public void setAwards(String awards) {
+		this.awards = awards;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public Rating[] getRatings() {
+		return ratings;
+	}
+	public void setRatings(Rating[] ratings) {
+		this.ratings = ratings;
+	}
+	public String getMetascore() {
+		return metascore;
+	}
+	public void setMetascore(String metascore) {
+		this.metascore = metascore;
+	}
+	public String getImdbRating() {
+		return imdbRating;
+	}
+	public void setImdbRating(String imdbRating) {
+		this.imdbRating = imdbRating;
+	}
+	public String getImdbID() {
+		return imdbID;
+	}
+	public void setImdbID(String imdbID) {
+		this.imdbID = imdbID;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDvd() {
+		return dvd;
+	}
+	public void setDvd(String dvd) {
+		this.dvd = dvd;
+	}
+	public String getBoxOffice() {
+		return boxOffice;
+	}
+	public void setBoxOffice(String boxOffice) {
+		this.boxOffice = boxOffice;
+	}
+	public String getProduction() {
+		return production;
+	}
+	public void setProduction(String production) {
+		this.production = production;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+	
+}
