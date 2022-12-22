3f25cb6313ebdc1e4de16f9e998e46dbe11a8ae3
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-22 16:41:37 -0500

    Initial commit

---
+++ src/main/java/com/coderscampus/ogulcanfinal/OgulcanFinalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ogulcanfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgulcanFinalAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgulcanFinalAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/ogulcanfinal/OgulcanFinalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ogulcanfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgulcanFinalAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
