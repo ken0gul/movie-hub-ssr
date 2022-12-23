5b4a5c4010a2351abe871e88a98f585d7c434855
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-23 16:44:47 -0500

    Add fetch api to consume the api getting movies

---
+++ src/main/resources/static/js/movie.js
@@ -0,0 +1,54 @@
+// Selecting elements
+let container = document.querySelector('.container');
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let movies = document.querySelector('.movies');
+
+button.addEventListener('click',() => {
+
+
+
+let movie = {};
+fetch(`http://www.omdbapi.com/?apikey=da216f55&t="${input.value}"`).then(response => {
+if(response.ok){
+    return response.json();
+}
+}).then(data => {
+    console.log(data)
+   movie = data;
+   if(data.Response == 'False') return;
+   let img = `
+   <div class="single-movie-container">
+   <h3>${data.Title}</h3>
+   <span>${data.Year}</span>
+   <img class="poster" src="${data.Poster}"/>
+   <button>See more!</button>
+   </div>
+   
+   
+   
+   
+   `;
+    
+   movies.innerHTML += img;
+
+   fetch('/movies',
+   {
+
+       method:'POST',
+       headers:{
+           'Content-Type':'application/JSON'
+       },
+       body:JSON.stringify(data)
+
+
+   }
+
+
+
+).then(response => response.json()).then(data => {
+   console.log(data)
+});
+});
+})
+
