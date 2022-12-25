7e063341db77ad316f16161de338a2c1ed542fb0
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-24 19:57:10 -0500

    Create UserRepository.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ogulcanfinal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coderscampus.ogulcanfinal.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findById(Long id);
+}
