4e888a3184a774d8b74bc459dcb13bb2c2830da2
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-24 19:56:44 -0500

    Create User.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/User.java
@@ -0,0 +1,82 @@
+package com.coderscampus.ogulcanfinal.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String username;
+	private String password;
+
+	@OneToMany(mappedBy = "user")
+	private List<Comment> comments;
+
+	@ManyToMany
+	@JoinTable(name = "movie_user",
+    joinColumns = @JoinColumn(name = "user_id"), 
+    inverseJoinColumns = @JoinColumn(name = "movie_id"))
+	private List<Movie> movies;
+	public Long getId() {
+		return userId;
+	}
+
+	public void setId(Long id) {
+		this.userId = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(List<Movie> movies) {
+		this.movies = movies;
+	}
+	
+	
+}
