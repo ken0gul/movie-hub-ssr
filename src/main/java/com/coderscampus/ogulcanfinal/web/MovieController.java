2f3413b8308c5c4d3d2ce9882e61ef36062a8a55
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-24 19:58:25 -0500

    Put user in the model map to render on html

--- src/main/java/com/coderscampus/ogulcanfinal/web/MovieController.java
+++ src/main/java/com/coderscampus/ogulcanfinal/web/MovieController.java
@@ -1,6 +1,5 @@
 package com.coderscampus.ogulcanfinal.web;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,68 +11,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.coderscampus.ogulcanfinal.domain.Comment;
 import com.coderscampus.ogulcanfinal.domain.Movie;
-import com.coderscampus.ogulcanfinal.model.CommentModel;
+import com.coderscampus.ogulcanfinal.domain.User;
 import com.coderscampus.ogulcanfinal.service.CommentService;
 import com.coderscampus.ogulcanfinal.service.MovieService;
+import com.coderscampus.ogulcanfinal.service.UserService;
 
 @Controller
 public class MovieController {
-	
+
 	@Autowired
 	private MovieService movieService;
-	
+
 	@Autowired
 	private CommentService commentService;
-	
-	
+
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/movies")
 	public String getMovies(ModelMap model) {
 		List<Movie> allMovies = movieService.findAll();
 		model.put("movies", allMovies);
+		
+		
+		List<User> users = userService.findAll();
+		
+		for(User user : users) {
+			
+			model.put("user", user);
+		}
+		
+
 		return "movies";
 	}
 
 	@PostMapping("/movies")
 	@ResponseBody
-	public Movie searchMovie(@RequestBody Movie movie) {
+	public Movie searchMovie(@RequestBody Movie movie, User user) {
+		
+		
 		return movieService.saveMovie(movie);
-		
-	}
-	
-	
-	@GetMapping("/movie/{id}")
-	public String getMovie(ModelMap model,@PathVariable Long id) {
-		Movie foundMovie = movieService.findById(id);
-		model.put("movie", foundMovie);
-		
-		
-//		CommentModel newComment = new CommentModel();
-//		model.put("comment", newComment);
-		
-		Comment newComment = new Comment();
-		model.put("comment", newComment);
 
-		
-		List<Comment> allComments = commentService.findAll();
-		List<Comment> comments = commentService.findByMovieId(id);
-		model.put("comments", comments);
-		
-		return "movie";
 	}
-	
-	@PostMapping("/movie/{id}")
-	public String postComment(Comment newComment, Movie movie) {
-		System.out.println(movie);
-		Comment comment = new Comment();
-		comment.setMovie(movie);
-		comment.setCommentText(newComment.getCommentText());
-		
-		Comment savedComment = commentService.saveComment(comment);
-		
-		
-		return "redirect:/movie/{id}";
-	}
-	
+
+
 }
