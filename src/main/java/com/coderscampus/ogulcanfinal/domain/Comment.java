a5cf2c7b8fdb150d6ecb3e938cfd282344bd95b0
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-24 19:56:07 -0500

    Add date as as instance variable

--- src/main/java/com/coderscampus/ogulcanfinal/domain/Comment.java
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/Comment.java
@@ -1,5 +1,7 @@
 package com.coderscampus.ogulcanfinal.domain;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,46 +11,61 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "comments")
+
 public class Comment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String commentText;
-	
-	
-	
+	private LocalDate date = LocalDate.now();
+
 	@ManyToOne
 	@JoinColumn(name = "movie_id")
 	private Movie movie;
-	
-	
-	
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getCommentText() {
 		return commentText;
 	}
+
 	public void setCommentText(String commentText) {
 		this.commentText = commentText;
 	}
+
 	public Movie getMovie() {
 		return movie;
 	}
+
 	public void setMovie(Movie movie) {
 		this.movie = movie;
 	}
-	
 
-	
-	
-	
+	public User getUser() {
+		return user;
+	}
 
-	
-	
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
 }
