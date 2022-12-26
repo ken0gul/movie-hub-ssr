6f5ad0797503d80bbcfcad8d02ed77682d245176
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-26 18:46:50 -0500

    Add controllers to get data from fetch api to mark movies as watched and delete from to-watch

--- src/main/java/com/coderscampus/ogulcanfinal/web/WatchListController.java
+++ src/main/java/com/coderscampus/ogulcanfinal/web/WatchListController.java
@@ -10,9 +10,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.coderscampus.ogulcanfinal.domain.Movie;
 import com.coderscampus.ogulcanfinal.domain.User;
+import com.coderscampus.ogulcanfinal.model.WatchedMovie;
+import com.coderscampus.ogulcanfinal.repository.UserRepository;
 import com.coderscampus.ogulcanfinal.service.MovieService;
 import com.coderscampus.ogulcanfinal.service.UserService;
 
@@ -30,6 +34,9 @@ public class WatchListController {
 	public String getUser(@PathVariable Long userId, ModelMap model) {
 		User user = userService.findById(userId);
 		model.put("user", user);
+		List<Movie> watchedMovies = user.getWatchedList();
+		watchedMovies.forEach(System.out::println);
+		model.put("movies", watchedMovies);
 		return "user";
 	}
 
@@ -46,8 +53,42 @@ public class WatchListController {
 
 		List<User> userList = new ArrayList<>();
 		userList.add(user);
-		movieFound.setUsers(userList);
+//		movieFound.setUsers(userList);
 		userService.save(user);
+		
 		return "redirect:/user/{userId}";
 	}
+	
+	// Watched
+	@GetMapping("/user/{userId}/delete")
+	public String getWatched(@PathVariable Long userId, ModelMap model) {
+		User user = userService.findById(userId);
+		System.out.println(user);
+		model.put("user", user);
+		
+		
+		return "user";
+	}
+	
+	
+	
+	@PostMapping("/user/{userId}/delete")
+	@ResponseBody
+	public WatchedMovie markAsWatched(@RequestBody WatchedMovie movie, @PathVariable Long userId) {
+		User user = userService.findById(userId);
+		System.out.println("Movie Id: " + movie.getMovieId());
+		System.out.println(user);
+		Movie foundMovie = movieService.findById(movie.getMovieId());
+		List<Movie> watchedList = new ArrayList<>();
+		foundMovie.setUser(user);
+		watchedList.add(foundMovie);
+		user.setWatchedList(watchedList);
+		
+		userService.save(user);
+		movieService.saveMovie(foundMovie);
+		userService.deleteMovieByMovieIdFromMovieUserWatch(movie.getMovieId());
+		return movie;
+	}
+	
+	
 }
