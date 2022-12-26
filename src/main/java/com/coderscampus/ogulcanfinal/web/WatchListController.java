5bbd7b1509461915ceabb79a6fecf8c2e12bfd43
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-25 19:10:43 -0500

    Create WatchListController.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/web/WatchListController.java
@@ -0,0 +1,68 @@
+package com.coderscampus.ogulcanfinal.web;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.coderscampus.ogulcanfinal.domain.Movie;
+import com.coderscampus.ogulcanfinal.domain.User;
+import com.coderscampus.ogulcanfinal.service.CommentService;
+import com.coderscampus.ogulcanfinal.service.MovieService;
+import com.coderscampus.ogulcanfinal.service.UserService;
+
+@Controller
+
+public class WatchListController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private MovieService movieService;
+
+	@Autowired
+	private CommentService commentService;
+
+	@GetMapping("/user/{userId}")
+	public String getUser(@PathVariable Long userId, ModelMap model) {
+		User user = userService.findById(userId);
+		model.put("user", user);
+		System.out.println(user);
+		return "user";
+	}
+//	
+//	@GetMapping("/user/{userId}/movie/{movieId}")
+//	public String getToWatch(ModelMap model) {
+//		return "user";
+//	}
+
+	@PostMapping("/user/{userId}")
+	public String postToDo(Movie movie, @PathVariable Long userId) {
+		
+		User user = userService.findById(userId);
+		System.out.println("Found User Post TO Do: " + user );
+		Long movieId = movie.getId();
+		Movie movieFound = movieService.findById(movieId);
+//		List<Movie> movieSet = new ArrayList<>();
+		List<Movie> watchList =  user.getWatchList();
+		watchList.add(movieFound);
+		
+		user.setWatchList(watchList);
+		
+		
+		List<User> userList = new ArrayList<>();
+		userList.add(user);
+		movieFound.setUsers(userList);
+		System.out.println("User" + user);
+		userService.save(user);
+		return "redirect:/user/{userId}";
+	}
+}
