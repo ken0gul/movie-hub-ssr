1d1af368b6adc74d3b952e0143a813219fc34477
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-26 18:33:37 -0500

    Create a model for WatchedMovie to act as an intermediary between server and db

---
+++ src/main/java/com/coderscampus/ogulcanfinal/model/WatchedMovie.java
@@ -0,0 +1,21 @@
+package com.coderscampus.ogulcanfinal.model;
+
+public class WatchedMovie {
+
+	private Long movieId;
+	private Long userId;
+	public Long getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
+	
+}
