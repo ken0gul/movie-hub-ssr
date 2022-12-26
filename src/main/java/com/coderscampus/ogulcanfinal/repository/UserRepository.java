aa23398b7e59bce87f89882c4189ac77758acf64
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-26 18:43:11 -0500

    Add a modifying query to delete movie by movieId

--- src/main/java/com/coderscampus/ogulcanfinal/repository/UserRepository.java
+++ src/main/java/com/coderscampus/ogulcanfinal/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.coderscampus.ogulcanfinal.repository;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.coderscampus.ogulcanfinal.domain.User;
@@ -10,4 +14,8 @@ import com.coderscampus.ogulcanfinal.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findById(Long id);
+	@Modifying
+	@Transactional
+	@Query(value="delete from movie_user_watch m where m.movie_id=:movieId",nativeQuery = true)
+	 void deleteMovieByMovieIdFromMovieUserWatch(Long movieId);
 }
