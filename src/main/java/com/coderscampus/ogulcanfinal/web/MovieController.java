5f799045fb510bcd18075b1b97e9360db4999d85
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-27 14:00:45 -0500

    Format the code

--- src/main/java/com/coderscampus/ogulcanfinal/web/MovieController.java
+++ src/main/java/com/coderscampus/ogulcanfinal/web/MovieController.java
@@ -1,12 +1,14 @@
 package com.coderscampus.ogulcanfinal.web;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,24 +26,20 @@ public class MovieController {
 	private MovieService movieService;
 
 	@Autowired
-	private CommentService commentService;
-
-	@Autowired
 	private UserService userService;
 
 	@GetMapping("/movies")
 	public String getMovies(ModelMap model) {
 		List<Movie> allMovies = movieService.findAll();
-		model.put("movies", allMovies);
-		
-		
+		Set<Movie> allUniqueMovies = allMovies.stream().collect(Collectors.toSet());
+		model.put("movies", allUniqueMovies);
+
 		List<User> users = userService.findAll();
-		
-		for(User user : users) {
-			
+
+		for (User user : users) {
+
 			model.put("user", user);
 		}
-		
 
 		return "movies";
 	}
@@ -49,11 +47,9 @@ public class MovieController {
 	@PostMapping("/movies")
 	@ResponseBody
 	public Movie searchMovie(@RequestBody Movie movie, User user) {
-		
-		
+
 		return movieService.saveMovie(movie);
 
 	}
-
 
 }
