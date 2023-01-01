7d4868188b7c8e556b01265c1df1f5a57c7c610d
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-01 14:21:50 -0500

    Create Authorities.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/Authorities.java
@@ -0,0 +1,51 @@
+package com.coderscampus.ogulcanfinal.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Authorities implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	@ManyToMany(mappedBy="authorities")
+	private Set<User> users = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return authority;
+	}
+
+}
