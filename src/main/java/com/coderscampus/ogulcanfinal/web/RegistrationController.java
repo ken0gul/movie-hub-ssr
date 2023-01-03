835e295be28565ff3e0e9145b900199efaee972f
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-02 20:12:59 -0500

    Add a test condition to check if username already exists

--- src/main/java/com/coderscampus/ogulcanfinal/web/RegistrationController.java
+++ src/main/java/com/coderscampus/ogulcanfinal/web/RegistrationController.java
@@ -1,7 +1,8 @@
 package com.coderscampus.ogulcanfinal.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,28 +16,40 @@ import com.coderscampus.ogulcanfinal.service.UserService;
 @Controller
 @RequestMapping("/users")
 public class RegistrationController {
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping("/register")
 	public String getRegistrationPage(ModelMap model) {
+		List<User> users = userService.findAll();
 		
 		model.put("user", new User());
+		
+		
 		return "registration";
 	}
-	
+
 	@PostMapping("/register")
 	public String createUser(User user) {
+
+		User foundUser = userService.findByUsername(user.getUsername());
+		
+		if(foundUser.getUsername().equals(user.getUsername())) {
+			System.out.println("yes");
+			return "redirect:/users/register";
+		} 
+		
+	
+		
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		
+
 		userService.save(user);
-		
+
 		return "redirect:/login";
 	}
-	
+
 }
