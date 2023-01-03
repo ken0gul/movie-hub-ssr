09517479e0fc9cb067c5605e0c97177922dfc47f
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-03 14:24:32 -0500

    Add a model attribute to model to display an error message when username already exists

--- src/main/java/com/coderscampus/ogulcanfinal/web/RegistrationController.java
+++ src/main/java/com/coderscampus/ogulcanfinal/web/RegistrationController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,8 +35,15 @@ public class RegistrationController {
 	}
 
 	@PostMapping("/register")
-	public String createUser(User user) {
-
+	public String createUser(@ModelAttribute User user, Model model) {
+		
+		boolean isUsernameExists = userService.usernameExists(user.getUsername());
+		
+		if(isUsernameExists) {
+			model.addAttribute("errorMessage","Username already exists");
+			return "registration";
+		}
+		
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userService.save(user);
 
