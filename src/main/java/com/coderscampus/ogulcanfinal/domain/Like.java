6472922e5a754ef78a5cc822cea8ddac913e4a1f
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-21 12:34:48 -0500

    Create Like.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/domain/Like.java
@@ -0,0 +1,42 @@
+package com.coderscampus.ogulcanfinal.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="likes")
+public class Like {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private Long userId;
+	@Column
+	private Long movieId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+	
+	
+}
