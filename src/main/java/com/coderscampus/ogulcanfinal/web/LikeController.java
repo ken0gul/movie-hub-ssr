80b0cbf764c71176ff2ca743d83b49517fd031da
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-21 12:35:10 -0500

    Create LikeController.java

---
+++ src/main/java/com/coderscampus/ogulcanfinal/web/LikeController.java
@@ -0,0 +1,43 @@
+package com.coderscampus.ogulcanfinal.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coderscampus.ogulcanfinal.service.LikeService;
+
+@RestController
+public class LikeController {
+
+	
+	@Autowired
+	private LikeService likeService;
+	
+	
+	
+	@PostMapping("/movie/{movieId}/user/{userId}/like")
+	public ResponseEntity<Long>like(@PathVariable Long movieId, @PathVariable Long userId){
+
+			
+		likeService.like(userId, movieId);
+		Long numberOfLikes = likeService.getNumberOfLikes(movieId);
+		System.out.println(numberOfLikes);
+		return ResponseEntity.ok(numberOfLikes);
+	
+		
+		
+	}
+	@PostMapping("/movie/{movieId}/user/{userId}/unlike")
+	public ResponseEntity<Long>unlike(@PathVariable Long movieId, @PathVariable Long userId){
+
+			
+		likeService.unlike(userId, movieId);
+		Long numberOfLikes = likeService.getNumberOfLikes(movieId);
+		return ResponseEntity.ok(numberOfLikes);
+	
+		
+		
+	}
+}
